5d8e3c9c805b51e746bb565fd1ddee9c228f2b86
Ryan <rnowusu@gwu.edu>
2019-02-22 16:31:21 -0500

    Updates

---
+++ precache-manifest.d02d87f4de521017bb193c769eeb6823.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb32a3088826a623705c",
+    "url": "/calitho-prototype/static/js/runtime~main.fb32a308.js"
+  },
+  {
+    "revision": "db0f8265c17da56f9171",
+    "url": "/calitho-prototype/static/js/main.db0f8265.chunk.js"
+  },
+  {
+    "revision": "aae41e998371a85b029d",
+    "url": "/calitho-prototype/static/js/2.aae41e99.chunk.js"
+  },
+  {
+    "revision": "db0f8265c17da56f9171",
+    "url": "/calitho-prototype/static/css/main.171b9351.chunk.css"
+  },
+  {
+    "revision": "e1aa991d19d58fcf38750ff4eb3e86ad",
+    "url": "/calitho-prototype/index.html"
+  }
+];
